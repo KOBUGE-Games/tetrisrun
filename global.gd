a1d600fe8a077e507abd3746f361533f6aa4734d
Maveyyl <martialveyry@hotmail.fr>
2017-11-09 20:12:16 +0100

    Early implementation of runner

---
+++ global.gd
@@ -0,0 +1,12 @@
+extends Node
+
+const runner_radius = 50
+const runner_circumference = 2 * PI * runner_radius
+const runner_speed = 10
+
+
+
+
+func _ready():
+	pass
+
--- runner/runner.gd
+++ runner/runner.gd
@@ -1,15 +1,27 @@
-extends KinematicBody2D
+extends RigidBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+
+
+var State = {
+	"Idle": 0,
+	"Running": 1,
+	"Dead": 2
+}
+var state = State.Idle
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	if( state == State.Running ):
+		linear_velocity = Vector2(global.runner_speed)
+
+
+func set_idle():
+	state = State.Idle
+func set_running():
+	state = State.Running
+func set_dead():
+	state = State.Dead
